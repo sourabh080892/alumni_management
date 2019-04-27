a327f0aaf23b0c52d02e317e8f0306f8cbf1ea2a
HarryDB <lehuy@seattleu.edu>
2019-04-27 00:37:28 -0700

    populate data script, it works well on my computer

---
+++ SQL_scripts/data_01_alum_program.sql
@@ -0,0 +1,27 @@
+Use segr5300
+
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500000, 300300, 30008, 2009);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500001, 300301, 30004, 1994);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500002, 300302, 30004, 1996);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500003, 300303, 30022, 2013);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500004, 300304, 30021, 1998);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500005, 300305, 30004, 1998);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500006, 300306, 30021, 1992);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500007, 300307, 30011, 2013);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500008, 300308, 30010, 2005);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500009, 300309, 30019, 2012);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500010, 300310, 30015, 1991);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500011, 300311, 30022, 2002);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500012, 300312, 30002, 2010);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500013, 300313, 30013, 2008);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500014, 300314, 30018, 2009);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500015, 300315, 30004, 1995);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500016, 300316, 30015, 2010);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500017, 300317, 30012, 1991);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500018, 300318, 30010, 2001);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500019, 300319, 30019, 2012);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500020, 300320, 30020, 1996);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500021, 300321, 30014, 2008);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500022, 300322, 30019, 2008);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500023, 300323, 30021, 1999);
+INSERT INTO alum_program (alum_program_id, alumni_id, program_Id, alum_program_grad_year) VALUES (500024, 300324, 30017, 2003);
