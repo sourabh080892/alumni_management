6e84856b8ea1053019dd81035f99617430080f8a
HarryDB <43583988+quochuy201@users.noreply.github.com>
2019-04-30 16:43:08 -0700

    Add files via upload

---
+++ SQL_scripts/data_01_programcategory.sql
@@ -0,0 +1,7 @@
+use segr5300;
+INSERT INTO program_categories 
+(program_categories_id, program_categories_name)
+VALUES ('GRAD','Graduate');
+INSERT INTO program_categories (program_categories_id, program_categories_name)
+ VALUES ('UNGR','Under Graduate');
+INSERT INTO program_categories (program_categories_id, program_categories_name) VALUES ('CERT','Certificate');
