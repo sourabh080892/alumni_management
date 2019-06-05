b0b702b76970b54be6e5d5841372602687f752d1
Sourabh Gupta <guptasourabh@seattleu.com>
2019-06-05 12:19:12 -0700

    dean use case tableau changes

--- SQL_scripts/milestone2/AnalyticalQuery_Dean_usecase.sql
+++ SQL_scripts/milestone2/AnalyticalQuery_Dean_usecase.sql
@@ -1,6 +1,6 @@
 use `segr5300`;
 
-# fetch top 3 cities where donation is maximum
+/* # fetch top 3 cities where donation is maximum
 select 	c.city_id, 
 		c.city_name,
         SUM(ad.alumni_donation_amount) as 'Total Donation',
@@ -27,7 +27,7 @@ inner join (select c.city_id as 'city_id', COUNT(a.alumni_id) as 'no_alumni'
 on c.city_id = ci_al.city_id
 group by c.city_name
 order by ci_co.no_comp desc, SUM(ad.alumni_donation_amount) desc
-limit 3;
+limit 3; */
 
 # After deciding the city, now we want all the details from that city
 select 	c.city_name as 'City Name',
@@ -36,7 +36,10 @@ select 	c.city_name as 'City Name',
         GROUP_CONCAT(ale.all_events_name SEPARATOR ', ') as 'Events Attended',
         p.program_name as 'Program Name',
         ad.alumni_donation_amount as 'Donation Amount',
-        co.company_name as 'Company Name'
+        co.company_name as 'Company Name',
+        city_3.adm as 'Total Donation',
+        city_3.noc as 'No of Companies',
+        city_3.noa as 'No of Alumni'
 from city as c
 inner join alumni as a
 on c.city_id = a.city_id
@@ -52,7 +55,34 @@ left outer join alumni_donation as ad
 on a.alumni_id = ad.alumni_id
 left outer join company as co
 on a.company_id = co.company_id
-where c.city_id = 1
+inner join (select 	c.city_id, 
+		c.city_name,
+        SUM(ad.alumni_donation_amount) as adm,
+        ci_co.no_comp as noc,
+        ci_al.no_alumni as noa
+from city as c
+inner join alumni as a
+on c.city_id = a.city_id
+inner join alumni_donation as ad
+on a.alumni_id = ad.alumni_id
+inner join (select c.city_id as 'city_id', COUNT(co.company_name) as 'no_comp'
+				from city as c
+				inner join alumni as a
+				on c.city_id = a.city_id
+				inner join company as co
+				on a.company_id = co.company_id
+				group by c.city_name) as ci_co
+on c.city_id = ci_co.city_id 
+inner join (select c.city_id as 'city_id', COUNT(a.alumni_id) as 'no_alumni'
+				from city as c
+				inner join alumni as a
+				on c.city_id = a.city_id
+				group by c.city_name) as ci_al
+on c.city_id = ci_al.city_id
+group by c.city_name
+order by ci_co.no_comp desc, SUM(ad.alumni_donation_amount) desc
+limit 3) as city_3
+on c.city_id = city_3.city_id
 group by a.alumni_name;
 
 /* # select no of companies in each city
